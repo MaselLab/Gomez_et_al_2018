b80ef248f6068fa9d300f7e12cfea0447a63c191
Kevin <kgomez81@math.arizona.edu>
2017-06-01 18:14:50 -0700

    added code for calculating derivatives of covariance

--- plots.py
+++ plots.py
@@ -15,59 +15,56 @@ import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_load_data(genotypes,abundances,trait_avgs,num_pts):
-    pop_load = np.zeros((num_pts,))
-    
-    for i in range(num_pts):
-        numb_of_genotypes = len(abundances[i][0])
-        L1 = np.amax((genotypes[i]-np.array([trait_avgs[i]-np.array([[1,0]]) for j in range(numb_of_genotypes)])).dot(np.array([[s1],[s2]])))
-        L2 = np.amax((genotypes[i]-np.array([trait_avgs[i]-np.array([[0,1]]) for j in range(numb_of_genotypes)])).dot(np.array([[s1],[s2]])))
-        pop_load[i] = max([L1,L2])
-    return pop_load
+# set parameters of simulation and create required variables
+N=1e9; s1=1e-2; s2=1e-2; U1=1e-5; U2=1e-5;
+vU_thry = s1*s1*(2*np.log(N*s1)-np.log(s1/(1*U1)))/((np.log(s1/(1*U1)))**2)
+v2U_thry = 0.5*s1*s1*(2*np.log(N*s1)-np.log(s1/(2*U1)))/((np.log(s1/(2*U1)))**2)
+tau_est = 0.5*s1/v2U_thry
+data_name = '_N-10p09_c1-0d01_c2-0d01_U1-1x10pn5_U2-1x10pn5_exp1'
+[times,genotypes,abundances] = [[],[],[]]
 
+# LOAD DATA FROM PICKLE FILE
+pickle_file_name = './Documents/kgrel2d/data/pythondata/pythondata'+data_name+'.pickle'
+pickle_file = open(pickle_file_name,'rb') 
+[times,genotypes,abundances,trait_avgs,variances,covariance,pop_load,var1_avg
+    ,var2_avg,cov_avg,unit_array,vU_thry,v2U_thry,num_pts,times_distr_cov
+    ,times_distr_roa,dcov_dt,frequencies] = pickle.load(pickle_file)
+pickle_file.close()
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 def get_sample_window(times,start_time,end_time):
-    num_pts = len(times)
-    start_indx = 0
-    end_indx = 0
+    [num_pts,start_indx,end_indx] = [len(times),0,0]    
     for i in range(len(times)):
         if times[start_indx] <= start_time:
             start_indx = start_indx + 1
         if times[end_indx] <= end_time:
             end_indx = end_indx + 1
     return [start_indx,end_indx]
-    
-def get_characteristic_time(covariance,times,percent_change):
-    times_distr = []
-    max_cov_change = percent_change*np.abs(np.mean(covariance))
-    indx1 = 0
-    indx2 = 0
-    max_loop = len(covariance)-1
-    
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------    
+def get_characteristic_time(meas_qty,times,percent_change):
+    [times_distr,indx1,indx2,max_loop] = [[],0,0,len(meas_qty)-1]
+    max_meas_qty_change = percent_change*np.abs(np.mean(meas_qty))
     while (indx1 < max_loop):
         indx2 = indx1        
         while (indx2 <= max_loop):
-            if (np.abs(covariance[indx1]-covariance[indx2])<= max_cov_change):
+            if (np.abs(meas_qty[indx1]-meas_qty[indx2])<= max_meas_qty_change):
                 indx2 = indx2 + 1
             else:
                 times_distr = times_distr+[times[indx2]-times[indx1]]
                 indx2 = max_loop + 1                
-        indx1 = indx1 + 1
-        
+        indx1 = indx1 + 1        
     return times_distr
-    
-# parameters of simulation
-N=1e9; s1=1e-2; s2=1e-2; U1=1e-5; U2=1e-5;
-vU_thry = s1*s1*(2*np.log(N*s1)-np.log(s1/(1*U1)))/((np.log(s1/(1*U1)))**2)
-v2U_thry = 0.5*s1*s1*(2*np.log(N*s1)-np.log(s1/(2*U1)))/((np.log(s1/(2*U1)))**2)
-tau_est = 0.5*s1/v2U_thry
-data_name = '_N-10p09_c1-0d01_c2-0d01_U1-1x10pn5_U2-1x10pn5_exp1'
-times = []; genotypes = []; abundances = []
-
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # get simulation data and store genotypes as lists since they vary in dimensions over time
 data_file=open('./Documents/kgrel2d/data/pythondata/times'+data_name+'.dat')
 times = data_file.read().splitlines()
 times = np.array(map(float,times))
-num_pts = len(times)
 data_file.close()
 data_file=open('./Documents/kgrel2d/data/pythondata/genotypes'+data_name+'.dat')
 genotypes = data_file.read().splitlines()
@@ -76,6 +73,7 @@ data_file=open('./Documents/kgrel2d/data/pythondata/abundances'+data_name+'.dat'
 abundances = data_file.read().splitlines()
 data_file.close()
 del data_file
+num_pts = len(times)
 
 # clean up mathematica data format
 for i in range(num_pts):
@@ -85,35 +83,35 @@ for i in range(num_pts):
     exec(genotypes[i])
     abundances[i]='abundances[i]=np.array([['+abundances[i].replace('\t',',')+']])'
     exec(abundances[i])
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------    
 
 # compute data for use in plots
-trait_avgs = np.zeros((num_pts,2))
-rel_fitness = genotypes
-frequencies = abundances
-covariance = np.zeros(np.shape(times))
-variances = np.zeros((num_pts,2))
-tot_variance = np.zeros(np.shape(times))
+[rel_fitness,frequencies] = [genotypes[:],abundances[:]]
+[trait_avgs,variances] = [np.zeros((num_pts,2)) for j in range(2)]
+[covariance,pop_load,dcov_dt] = [np.zeros(np.shape(times)) for j in range(3)]
 
 for i in range(num_pts):
+    numb_genotypes = len(abundances[i][0])    
     frequencies[i] = (1/np.sum(frequencies[i]))*frequencies[i]
-    trait_avgs[i] = abundances[i].dot(genotypes[i])[0]
-    rel_fitness[i] = (genotypes[i]-np.array([trait_avgs[i] for j in range(len(genotypes[i]))]))*np.array([[s1,s2] for j in range(len(genotypes[i]))])
+    trait_avgs[i] = frequencies[i].dot(genotypes[i])[0]
+    rel_fitness[i] = rel_fitness[i]-np.array([trait_avgs[i] for j in range(numb_genotypes)])
+    rel_fitness[i] = rel_fitness[i]*np.array([[s1,s2] for j in range(numb_genotypes)])
     variances[i] = (frequencies[i].dot(((rel_fitness[i])**2)))[0]
     covariance[i] = frequencies[i].dot(rel_fitness[i][:,0]*rel_fitness[i][:,1])
-    tot_variance[i] = variances[i][0]+variances[i][1]+2*covariance[i]
+    dcov_dt[i] = frequencies[i].dot(rel_fitness[i][:,0]**2*rel_fitness[i][:,1]+rel_fitness[i][:,1]**2*rel_fitness[i][:,0])
+    
+    L1 = np.amax((rel_fitness[i]-np.array([[s1,0] for j in range(numb_genotypes)])).dot(np.array([[1],[1]])))
+    L2 = np.amax((rel_fitness[i]-np.array([[0,s2] for j in range(numb_genotypes)])).dot(np.array([[1],[1]])))
+    pop_load[i] = max([L1,L2])
 
-del rel_fitness
-
-pop_load = get_load_data(genotypes,abundances,trait_avgs,num_pts)
-mid_pt_times = 0.5*(times[2:-1]+times[1:-2])
-spd_of_evol = np.transpose(np.array([((times[2:-1]-times[1:-2])**(-1)),((times[2:-1]-times[1:-2])**(-1))]))*(trait_avgs[2:-1]-trait_avgs[1:-2])
-
-# select indices for plots
+# select indices for plots and calculate means
 [start_indx,end_indx] = get_sample_window(times,1e4,2e4)
-unit_array = np.ones(np.shape(times[start_indx:end_indx])) 
-var1_avg = np.mean(variances[start_indx:end_indx,0])
-var2_avg = np.mean(variances[start_indx:end_indx,1])
+[var1_avg,var2_avg] = [np.mean(variances[start_indx:end_indx,i]) for i in range(2)] 
 cov_avg = np.mean(covariance)
+unit_array = np.ones(np.shape(times[start_indx:end_indx])) 
 
 # compute characteristic times of covariance and rate of adaptation
 times_distr_cov = get_characteristic_time(covariance,times,0.1)
@@ -121,23 +119,22 @@ times_distr_roa = get_characteristic_time(variances[:,0]+covariance,times,0.1)
 time_scale_cov = np.median(times_distr_cov)
 time_scale_roa = np.median(times_distr_roa)
 
-# dump data into pickle file
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+# DUMP DATA INTO PICKLE FILE
 pickle_file_name = './Documents/kgrel2d/data/pythondata/pythondata'+data_name+'.pickle'
 pickle_file = open(pickle_file_name,'wb') 
-pickle.dump([times,genotypes,abundances,trait_avgs,variances,covariance,tot_variance,pop_load
-            ,mid_pt_times,spd_of_evol,var1_avg,var2_avg,cov_avg,unit_array,vU_thry,v2U_thry,
-            num_pts,times_distr_cov,times_distr_roa],pickle_file,pickle.HIGHEST_PROTOCOL)
+pickle.dump([times,genotypes,abundances,trait_avgs,variances,covariance,pop_load
+            ,var1_avg,var2_avg,cov_avg,unit_array,vU_thry,v2U_thry,num_pts,
+            times_distr_cov,times_distr_roa,dcov_dt,frequencies],pickle_file,pickle.HIGHEST_PROTOCOL)
 pickle_file.close()
 
-# load data from pickle file
-pickle_file_name = './Documents/kgrel2d/data/pythondata/pythondata'+data_name+'.pickle'
-pickle_file = open(pickle_file_name,'rb') 
-[times,genotypes,abundances,trait_avgs,variances,covariance,tot_variance,pop_load
-        ,mid_pt_times,spd_of_evol,var1_avg,var2_avg,cov_avg,unit_array,vU_thry
-        ,v2U_thry,num_pts,times_distr_cov,times_distr_roa] = pickle.load(pickle_file)
-pickle_file.close()
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
-# paper figures
+# GENERATE FIGURES
 
 # figure 1: comparison of variances, covariance and rate of adaptation
 fig1,ax1=plt.subplots(nrows=1,ncols=1,figsize=[8,8])
