8ef21d5d35bdd4d6e2dadb435fbe679e2c31cf3f
Kevin Gomez <kgomez81@math.arizona.edu>
2017-10-24 16:34:55 -0700

    More updates

--- plots.py
+++ plots.py
@@ -146,30 +146,31 @@ NsUparam[start1:end1,1]=np.round(NsUparam[start1:end1,1],4)
 param_set = [min_par + i*(max_par-min_par)/100 for i in range(101)]
 param_vchg = [pltfun.get_vNsU_perChg(N,param_set[i],U,2) for i in range(101)]
 
-my_xticks = [(min_par + i*(max_par-min_par)/5.0) for i in range(6)]
-my_xlabel = [1e2*my_xticks[i] for i in range(len(my_xticks))]
+my_xticks = [0.1e-2, 0.3e-2, 0.5e-2, 0.7e-2, 0.9e-2, 1.1e-2]
+my_xlabel = ['0.1', '0.3', '0.5', '0.7', '0.9', '1.1']
+
 my_yticks = [20*i for i in range(9)]
 my_ylabel = ['']+[str(20*(i+1))+'%' for i in range(9)]
 
 fig2d, ax2d = plt.subplots(1,1,figsize=[8,8])
 #fig2d.subplots_adjust(bottom=0.25)
 fig2d.subplots_adjust(left=0.15)        
-ax2d.plot(np.asarray(param_set),100*np.asarray(param_vchg),c="black",label='$\%\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')                
-ax2d.plot(NsUparam[start1:end1,1],100*np.asarray(varp[start1:end1])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',linewidth=3.0,linestyle = '--')
-ax2d.plot(NsUparam[start1:end1,1],-100*np.asarray(covp[start1:end1]),c="black",label='$\%\Delta$|cov($r_1,r_2$)|',linewidth=3.0,linestyle = ':')        
+ax2d.plot(np.asarray(param_set),100*np.asarray(param_vchg),c="black",label='$\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')                
+ax2d.scatter(NsUparam[start1:end1,1],100*np.asarray(varp[start1:end1])-100,c="black",label='$\Delta \sigma_1^2$',s=40,marker = 'o')
+ax2d.scatter(NsUparam[start1:end1,1],-100*np.asarray(covp[start1:end1]),c="black",label='|$\sigma_{12}$|',s=40,marker = 'D')        
 ax2d.axhline(linewidth=0.5, color = 'k')     
 ax2d.set_ylabel(r'Scaled Variance-Covariance',fontsize=18)
 ax2d.set_xlabel(r'Selection Coefficient',fontsize=18)
 ax2d.tick_params(axis='both',labelsize=18)
 plt.xticks(my_xticks,my_xlabel)
 plt.yticks(my_yticks,my_ylabel)
-plt.annotate(r'$\times 10^{-2}$',xy=(505,20),xycoords='figure points',fontsize=20)
-plt.annotate('(a)',xy=(105,475),xycoords='figure points',fontsize=20)
+plt.annotate(r'$\times 10^{-2}$',xy=(505,35),xycoords='figure points',fontsize=20)
+plt.annotate('(a)',xy=(115,485),xycoords='figure points',fontsize=20)
 ax2d.grid(b='off', which='both', axis='both')
 #ax2d.ticklabel_format(style='plain',axis='x')
 ax2d.set_ylim((0,160))
 ax2d.set_xlim((min_par,max_par))
-ax2d.legend(loc=4, ncol=2,fontsize=16)
+ax2d.legend(loc=4, ncol=3,fontsize=16,frameon=True)
 #ax2d.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),fancybox=True, shadow=True, ncol=2,fontsize=20)
 fig2d.savefig('./figures/fig2d.pdf')
 
@@ -180,30 +181,31 @@ fig2d.savefig('./figures/fig2d.pdf')
 param_set = [min_par + i*(max_par-min_par)/100.0 for i in range(101)]
 param_vchg = [pltfun.get_vNsU_perChg(N,s,param_set[i],2) for i in range(101)]
 
-my_xticks = [(min_par + i*(max_par-min_par)/5.0) for i in range(6)]
-my_xlabel = [1e5*my_xticks[i] for i in range(len(my_xticks))]
+my_xticks = [0.1e-5, 0.3e-5, 0.5e-5, 0.7e-5, 0.9e-5, 1.1e-5]
+my_xlabel = ['0.1', '0.3', '0.5', '0.7', '0.9', '1.1']
+
 my_yticks = [20*i for i in range(9)]
 my_ylabel = ['']+[str(20*(i+1))+'%' for i in range(9)]
 
 fig2e, ax2e = plt.subplots(1,1,figsize=[8,8])
 #fig2e.subplots_adjust(bottom=0.25)        
 fig2e.subplots_adjust(left=0.15)
-ax2e.plot(np.asarray(param_set),100*np.asarray(param_vchg),c="black",label='$\%\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')
-ax2e.plot(NsUparam[start2:end2,2],100*np.asarray(varp[start2:end2])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',linewidth=3.0,linestyle = '--')
-ax2e.plot(NsUparam[start2:end2,2],-100*np.asarray(covp[start2:end2]),c="black",label='$\%\Delta$|cov($r_1,r_2$)|',linewidth=3.0,linestyle = ':')
+ax2e.plot(np.asarray(param_set),100*np.asarray(param_vchg),c="black",label='$\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')
+ax2e.scatter(NsUparam[start2:end2,2],100*np.asarray(varp[start2:end2])-100,c="black",label='$\Delta\sigma_1^2$',s=40,marker = 'o')
+ax2e.scatter(NsUparam[start2:end2,2],-100*np.asarray(covp[start2:end2]),c="black",label='$|\Delta\sigma_{12}$|',s=40,marker = 'D')
 ax2e.axhline(linewidth=0.5, color = 'k')
 ax2e.set_ylabel(r'Scaled Variance-Covariance',fontsize=18)
 ax2e.set_xlabel(r'Mutation Rate',fontsize=18)
 ax2e.tick_params(axis='both',labelsize=18)
 plt.xticks(my_xticks,my_xlabel)
 plt.yticks(my_yticks,my_ylabel)
-plt.annotate(r'$\times 10^{-5}$',xy=(505,20),xycoords='figure points',fontsize=20)
-plt.annotate('(b)',xy=(105,475),xycoords='figure points',fontsize=20)
+plt.annotate(r'$\times 10^{-5}$',xy=(505,35),xycoords='figure points',fontsize=20)
+plt.annotate('(b)',xy=(115,485),xycoords='figure points',fontsize=20)
 ax2e.grid(b='off', which='both', axis='both')
 #ax2e.ticklabel_format(style='plain',axis='x')
 ax2e.set_ylim((0,160))
 ax2e.set_xlim((min_par,max_par))  
-ax2e.legend(loc=4,ncol=2,fontsize=16)        
+ax2e.legend(loc=4,ncol=3,fontsize=16,frameon=True)        
 #ax2e.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),fancybox=True, shadow=True, ncol=2,fontsize=20)
 fig2e.savefig('./figures/fig2e.pdf')
 
@@ -215,30 +217,31 @@ NsUparam[start3:end3,0]=np.round(NsUparam[start3:end3,0],8)
 param_set = [min_par + i*(max_par-min_par)/100 for i in range(101)]
 param_vchg = [pltfun.get_vNsU_perChg(param_set[i],s,U,2) for i in range(101)]
 
-my_xticks = [(min_par + i*(max_par-min_par)/5.0) for i in range(6)]
-my_xlabel = [np.round(1e-8*my_xticks[i],2) for i in range(len(my_xticks))]
+my_xticks = [1.0e8, 7.0e8, 13.0e8, 19.0e8, 25.0e8]
+my_xlabel = ['1.0', '7.0', '13.0', '19.0', '25.0']
+
 my_yticks = [20*i for i in range(9)]
 my_ylabel = ['']+[str(20*(i+1))+'%' for i in range(9)]
 
 fig2f, ax2f = plt.subplots(1,1,figsize=[8,8])
 #fig2f.subplots_adjust(bottom=0.25)
 fig2f.subplots_adjust(left=0.15)
-ax2f.plot(np.asarray(param_set),100*np.asarray(param_vchg),c="black",label='$\%\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')
-ax2f.plot(NsUparam[start3:end3,0],100*np.asarray(varp[start3:end3])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',linewidth=3.0,linestyle = '--')
-ax2f.plot(NsUparam[start3:end3,0],-100*np.asarray(covp[start3:end3]),c="black",label='$\%\Delta$|cov($r_1,r_2$)|',linewidth=3.0,linestyle = ':')                
+ax2f.plot(np.asarray(param_set),100*np.asarray(param_vchg),c="black",label='$\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')
+ax2f.scatter(NsUparam[start3:end3,0],100*np.asarray(varp[start3:end3])-100,c="black",label='$\Delta\sigma_1^2$',s=40,marker = 'o')
+ax2f.scatter(NsUparam[start3:end3,0],-100*np.asarray(covp[start3:end3]),c="black",label='|$\Delta\sigma_{12}$|',s=40,marker = 'D')                
 ax2f.axhline(linewidth=0.5, color = 'k')           
 ax2f.set_ylabel(r'Scaled Variance-Covariance',fontsize=18)
 ax2f.set_xlabel(r'Population Size',fontsize=18)
 ax2f.tick_params(axis='both',labelsize=18)
 plt.xticks(my_xticks,my_xlabel)
 plt.yticks(my_yticks,my_ylabel)
-plt.annotate(r'$\times 10^{8}$',xy=(505,20),xycoords='figure points',fontsize=20)
-plt.annotate('(c)',xy=(105,475),xycoords='figure points',fontsize=20)
+plt.annotate(r'$\times 10^{8}$',xy=(505,35),xycoords='figure points',fontsize=20)
+plt.annotate('(c)',xy=(115,485),xycoords='figure points',fontsize=20)
 ax2f.grid(b='off', which='both', axis='both')
 #ax2f.ticklabel_format(style='plain',axis='x')
 ax2f.set_ylim((0,160))
 ax2f.set_xlim((min_par,max_par)) 
-ax2f.legend(loc=4, ncol=2,fontsize=16)        
+ax2f.legend(loc=4, ncol=3,fontsize=16,frameon=True)        
 #ax2f.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),fancybox=True, shadow=True, ncol=2,fontsize=20)        
 fig2f.savefig('./figures/fig2f.pdf')
 
@@ -277,11 +280,11 @@ ax2g.set_xlabel('Number of Traits Added',fontsize=18)
 #ax2g.tick_params(axis='both',labelsize=18)
 plt.xticks(my_xticks,my_xlabel)
 plt.yticks(my_yticks,my_ylabel)
-plt.annotate('(d)',xy=(105,475),xycoords='figure points',fontsize=20) 
+plt.annotate('(d)',xy=(115,485),xycoords='figure points',fontsize=20) 
 ax2g.grid(b='off', which='both', axis='both')
 ax2g.set_ylim((20,80))
 ax2g.set_xlim(0,10) 
-ax2g.legend(loc=4,ncol=2,fontsize=14)        
+ax2g.legend(loc=4,ncol=1,fontsize=16,frameon=True)        
 #ax2g.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),fancybox=True, shadow=True, ncol=2,fontsize=20)        
 fig2g.savefig('./figures/fig2g.pdf')
 
@@ -331,14 +334,23 @@ param_vchg_N = [pltfun.get_vNsU_perChg(param_set_N[i],s,U,2) for i in range(101)
 
 [spcint,spcflt] = [6,5.0]
 
-my_xticks_s = [(min_par_s + i*(max_par_s-min_par_s)/spcflt) for i in range(spcint)]
-my_xlabel_s = [str(1e2*my_xticks_s[i]) for i in range(len(my_xticks_s))]
+#my_xticks_s = [(min_par_s + i*(max_par_s-min_par_s)/spcflt) for i in range(spcint)]
+#my_xlabel_s = [str(1e2*my_xticks_s[i]) for i in range(len(my_xticks_s))]
+#
+#my_xticks_U = [(min_par_U + i*(max_par_U-min_par_U)/spcflt) for i in range(spcint)]
+#my_xlabel_U = [str(1e5*my_xticks_U[i]) for i in range(len(my_xticks_U))]
+#
+#my_xticks_N = [(min_par_N + i*(max_par_N-min_par_N)/spcflt) for i in range(spcint)]
+#my_xlabel_N = [str(1e-8*my_xticks_N[i]) for i in range(len(my_xticks_N))]
 
-my_xticks_U = [(min_par_U + i*(max_par_U-min_par_U)/spcflt) for i in range(spcint)]
-my_xlabel_U = [str(1e5*my_xticks_U[i]) for i in range(len(my_xticks_U))]
+my_xticks_s = [0.1e-2, 0.3e-2, 0.5e-2, 0.7e-2, 0.9e-2, 1.1e-2]
+my_xlabel_s = ['0.1', '0.3', '0.5', '0.7', '0.9', '1.1']
 
-my_xticks_N = [(min_par_N + i*(max_par_N-min_par_N)/spcflt) for i in range(spcint)]
-my_xlabel_N = [str(1e-8*my_xticks_N[i]) for i in range(len(my_xticks_N))]
+my_xticks_U = [0.1e-5, 0.3e-5, 0.5e-5, 0.7e-5, 0.9e-5, 1.1e-5]
+my_xlabel_U = ['0.1', '0.3', '0.5', '0.7', '0.9', '1.1']
+
+my_xticks_N = [1.0e8, 7.0e8, 13.0e8, 19.0e8, 25.0e8]
+my_xlabel_N = ['1.0', '7.0', '13.0', '19.0', '25.0']
 
 my_yticks = [20*i for i in range(9)]
 my_ylabel = ['']+[str(20*(i+1))+'%' for i in range(9)]
@@ -347,15 +359,14 @@ my_ylabel = ['']+[str(20*(i+1))+'%' for i in range(9)]
 #fig2 = plt.subplots(1,3,figsize=[24,8])
 
 fig=plt.figure(figsize=[24,8])
-
 ax=plt.subplot(131)
 ax.plot(np.asarray(param_set_s),100*np.asarray(param_vchg_s),c="black",label='$\%\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')                
-ax.scatter(NsUparam[start1:end1,1],100*np.asarray(varp[start1:end1])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',s=20.0,marker = 'D')
-ax.scatter(NsUparam[start1:end1,1],-100*np.asarray(covp[start1:end1]),label='$\%\Delta$|cov($r_1,r_2$)|',s=20.0,marker = 'o')        
-plt.annotate(r'$\times 10^{-2}$',xy=(540,20),xycoords='figure points',fontsize=20)
-ax.set_ylabel(r'Scaled Variance-Covariance',fontsize=18)
-ax.set_xlabel(r'Selection Coefficient',fontsize=18)
-#ax2d.legend(loc=4, ncol=2,fontsize=16)
+ax.scatter(NsUparam[start1:end1,1],100*np.asarray(varp[start1:end1])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',s=40.0,marker = 'D')
+ax.scatter(NsUparam[start1:end1,1],-100*np.asarray(covp[start1:end1]),label='$\%\Delta$|cov($r_1,r_2$)|',s=40.0,marker = 'o')        
+plt.annotate(r'$\times 10^{-2}$',xy=(585,38),xycoords='figure points',fontsize=20)
+ax.set_ylabel(r'Scaled Variance-Covariance',fontsize=20)
+ax.set_xlabel(r'Selection Coefficient',fontsize=20)
+ax.tick_params(axis='both',labelsize=20)
 ax.set_ylim((0,160))
 ax.set_xlim((min_par_s,max_par_s))
 plt.yticks(my_yticks,my_ylabel)
@@ -363,25 +374,28 @@ plt.xticks(my_xticks_s,my_xlabel_s)
 
 ax=plt.subplot(132)
 ax.plot(np.asarray(param_set_U),100*np.asarray(param_vchg_U),c="black",label='$\%\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')
-ax.scatter(NsUparam[start2:end2,2],100*np.asarray(varp[start2:end2])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',s=20.0,marker = 'D')
-ax.scatter(NsUparam[start2:end2,2],-100*np.asarray(covp[start2:end2]),label='$\%\Delta$|cov($r_1,r_2$)|',s=20.0,marker = 'o')
-plt.annotate(r'$\times 10^{-2}$',xy=(1100,20),xycoords='figure points',fontsize=20)
-ax.set_xlabel(r'Mutation Rate',fontsize=18)
+ax.scatter(NsUparam[start2:end2,2],100*np.asarray(varp[start2:end2])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',s=40.0,marker = 'D')
+ax.scatter(NsUparam[start2:end2,2],-100*np.asarray(covp[start2:end2]),label='$\%\Delta$|cov($r_1,r_2$)|',s=40.0,marker = 'o')
+plt.annotate(r'$\times 10^{-5}$',xy=(1055,38),xycoords='figure points',fontsize=20)
+ax.legend(loc=4, ncol=2,fontsize=16)
+ax.set_xlabel(r'Mutation Rate',fontsize=20)
 ax.set_xlim((min_par_U,max_par_U))
+ax.tick_params(axis='both',labelsize=20)
 plt.yticks(my_yticks,[])
 fig2=plt.xticks(my_xticks_U,my_xlabel_U)
 
 ax=plt.subplot(133)
 ax.plot(np.asarray(param_set_N),100*np.asarray(param_vchg_N),c="black",label='$\%\Delta$v$_{1}$',linewidth=3.0,linestyle = '-')
-ax.scatter(NsUparam[start3:end3,0],100*np.asarray(varp[start3:end3])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',s=20.0,marker = 'D')
-ax.scatter(NsUparam[start3:end3,0],-100*np.asarray(covp[start3:end3]),label='$\%\Delta$|cov($r_1,r_2$)|',s=20.0,marker = 'o')                
-plt.annotate(r'$\times 10^{-2}$',xy=(1560,20),xycoords='figure points',fontsize=20)
+ax.scatter(NsUparam[start3:end3,0],100*np.asarray(varp[start3:end3])-100,c="black",label='$\%\Delta$var($r_1|r_2$)',s=40.0,marker = 'D')
+ax.scatter(NsUparam[start3:end3,0],-100*np.asarray(covp[start3:end3]),label='$\%\Delta$|cov($r_1,r_2$)|',s=40.0,marker = 'o')                
+plt.annotate(r'$\times 10^{8}$',xy=(1525,38),xycoords='figure points',fontsize=20)
 ax.set_xlabel(r'Population Size',fontsize=18)
 ax.set_xlim((min_par_N,max_par_N))
+ax.tick_params(axis='both',labelsize=20)
 plt.yticks(my_yticks,[])
 plt.xticks(my_xticks_N,my_xlabel_N)
 
-fig.subplots_adjust(wspace=0.1)
+fig.subplots_adjust(wspace=0.2)
 plt.tight_layout
 plt.savefig('./figures/fig2a.pdf',bbox_inches='tight')
 
